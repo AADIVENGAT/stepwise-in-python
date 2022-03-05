74e6fed84de1b82d5b8111e5a90d85ed4a900515
AADIVENGAT <88031195+AADIVENGAT@users.noreply.github.com>
2022-03-05 07:08:17 +0530

    Add files via upload

---
+++ march3.15.py
@@ -0,0 +1,24 @@
+#stepwise :
+from turtle import*
+begin_fill
+color("pink")
+
+backward(100)
+left(90)
+forward(100)
+right(90)
+backward(100)
+left(90)
+forward(100)
+right(90)
+
+backward(100)
+left(90)
+forward(100)
+right(90)
+backward(100)
+left(90)
+forward(100)
+right(90)
+
+end_fill 
